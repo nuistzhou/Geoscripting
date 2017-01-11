95b75d44cd7574979abc0d6a03de1f41877e747f
Almeida <rodrigo.almeida@wur.nl>
2017-01-11 14:48:32 +0100

    Function definition

--- Lesson3/R/leap.R
+++ Lesson3/R/leap.R
@@ -1,3 +1,25 @@
 # is.leap function definition
 
-is.leap <- 
+is.leap <- function(year) {
+  if (is.numeric(year) && year > 0){
+    if (year %% 4 != 0){
+      return(FALSE)
+    }
+    else if (year %% 100 != 0){
+      return(TRUE)
+    }
+    else if (year %% 400 != 0){
+      return(FALSE)
+    }
+    else {
+      return(TRUE)
+    }
+  }
+  else if (is.numeric(0)) {
+    return(paste("Warning:", year, "is out of the valid range."))
+  }
+  else {
+    return("Error: argument of class numeric expected")
+  }
+}
+
