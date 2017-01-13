36e9a41a07d82fbc510372bece3ec02aef07b84d
nuistzhou <nuistzhou@gmail.com>
2017-01-13 16:41:14 +0100

    Added some functions

--- Lesson5/R/ndvi_diff.R
+++ Lesson5/R/ndvi_diff.R
@@ -1,7 +1,7 @@
 # calculate NDVI difference between 2 date
-diff_ndvi <- function(input1,input2){
+diff_ndvi <- function(input1,input2,r1,r2,i1,i2){
   #RED band to be 1st and IR to be 2nd
-  ndvi1 <- (input1[2]-input1[1])/(input1[2]+input1[1])
-  ndvi2 <- (input2[2]-input2[1])/(input2[2]+input2[1])
-  return ndvi2-ndvi1
+  ndvi1 <- (input1[i1]-input1[r1])/(input1[i1]+input1[r1])
+  ndvi2 <- (input2[i2]-input2[r2])/(input2[i2]+input2[r2])
+  return (ndvi2-ndvi1)
 }
