36973f2b73b35114cb07fd62fa97e5e85c48bf4f
rodrigoalmeida94 <rodrigo.almeida94@outlook.pt>
2017-01-13 15:30:44 +0100

    Fix download function

--- Lesson5/R/download.R
+++ Lesson5/R/download.R
@@ -2,16 +2,28 @@
 # Team Dragon Masters
 # 13/01/2017
 
-download_date <- function(URL){
+#In the dropbox link, please use ?dl=1 in the end of the URL instead of ?dl=0
+download <- function(URL){
   # Creates data directory if not present
   system('mkdir -p data')
+  tar_file <- regmatches(URL, regexec('L.+.tar.gz', URL))
+  scene_name <- regmatches(URL, regexec('L.{32}', URL))
   # Downloads the file in data directory
-  system(paste('curl -o', getwd(), '/data', URL))
-  #Uses the URL to capture the year of acquisition according to LANDSAT scene name convention
-  #
-  year <- grep(pattern = '(?:L.{8})(\d{4})(?:.*\.tar\.gz)', URL)
-  #Uses the Usame process to capture the julian year of acquisition
-  day_year <- grep(pattern = '(?:L.{12})(\d{3})(?:.*\.tar\.gz)', URL)
-  #Returns the year and julian day of acquisition
+  download.file(URL, tar_file[[1]])
+  # Untars the file in the folder
+  untar(tar_file[[1]], exdir = paste('data/',scene_name[[1]],sep=''))
+  # Removes tar file
+  system(paste('rm',tar_file[[1]]))
+  # Returns downloaded file name
+  return(scene_name[[1]])
+}
+
+date_acquisition <- function(scene_name){
+  # Uses the filename to capture the year of acquisition according to LANDSAT scene name convention
+  # http://gisgeography.com/landsat-file-naming-convention/
+  year <- regexpr('(?:L.{8})(.{4})(?:.*\\.tar\\.gz)', filename)
+  # Uses the same process to capture the julian year of acquisition
+  day_year <- regexpr('(?:L.{12})(.{3})(?:.*\\.tar\\.gz)', filename)
+  # Returns the year and julian day of acquisition
   return(c(year,day_year))
 }
