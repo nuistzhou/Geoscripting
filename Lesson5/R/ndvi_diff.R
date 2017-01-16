2d0107f21b6e19faa8ad227253a7501760c8d9f2
nuistzhou <nuistzhou@gmail.com>
2017-01-16 09:51:13 +0100

    change an error

--- Lesson5/R/ndvi_diff.R
+++ Lesson5/R/ndvi_diff.R
@@ -4,7 +4,7 @@
 
 # calculate NDVI difference between 2 date
 # two inputs are stacks of 2 date
-ndvi_diff <- function(c(input1,input2)) {
+ndvi_diff <- function(input1,input2) {
   #RED band is 1st layer and Infra Red is 2nd layer
   ndvi1 <- ((input1[[2]]-input1[[1]])/(input1[[2]]+input1[[1]]))
   ndvi2 <- ((input2[[2]]-input2[[1]])/(input2[[2]]+input2[[1]]))
