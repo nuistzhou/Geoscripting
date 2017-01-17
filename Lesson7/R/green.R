3326f45470b262868f2a52d7ae135d084bb2d48d
nuistzhou <nuistzhou@gmail.com>
2017-01-17 17:23:00 +0100

    updated

--- Lesson7/R/green.R
+++ Lesson7/R/green.R
@@ -2,17 +2,16 @@
 # Team Dragon Masters
 # 17/01/2017
 
-# Greenest municipality
-
-green_month <- function(month, r_stack, regions){
-  r_subset <- subset(r_stack, month)
-  r_subset_masked <- mask(r_subset, regions)
-  green_index <- extract(r_subset_masked, regions, fun = mean,df=T )
+# Greenest regions(municipality/provinces)
+green_data <- function(r_stack, regions){
+  r_masked <- mask(r_stack, regions)
+  green_index <- extract(r_masked, regions, fun = mean,df=T )
   result <- cbind(regions@data, green_index)
+  result$year <- rowMeans(test[17:28], na.rm = T)
   return(result)
 }
 
-green_city <- function(dataframe, month){
-  id <- which.max(dataframe[[month]])
+green_city <- function(dataframe, period){
+  id <- which.max(dataframe[[period]])
   return(dataframe[id,]$NAME_2)
 }
--- Lesson7/main.R
+++ Lesson7/main.R
@@ -19,17 +19,28 @@ modis <- stack(modis_file)
 # Transforms the municipality dataset into the CRS of modis ndvi
 nlMunicipality <- spTransform(nlMunicipality, CRS(proj4string(modis)))
 
-# Crops to the nhetherlands extent for efficency 
+# Crops to the netherlands extent for efficency 
 modis <- crop(modis, nlMunicipality)
 
-# TO FUNCTION
-# Greenest municipality in January
-green_jan <- green_month('January',modis, nlMunicipality)
-print(paste('The greenest municipality in January is',green_city(green_jan, 'January')))
+# Create the data frame with NDVI for 12 months in whole regions
+green <- green_data(modis,nlMunicipality)
 
 # Greenest municipality in January
-green_aug <- green_month('August',modis, nlMunicipality)
-print(paste('The greenest municipality in August is',green_city(green_aug, 'August')))
+print(paste('The greenest municipality in January is',green_city(green, 'January')))
 
+# Greenest municipality in August
+print(paste('The greenest municipality in August is',green_city(green, 'August')))
 
+# Greenest municipality in the year
+print(paste('The greenest municipality in the year is',green_city(green, 'year')))
+
+# Gets the values of NDVI into the polygons data set
+nlMunicipality@data$year <- green$year
+nlMunicipality@data$January <- green$January
+nlMunicipality@data$August <- green$August
+
+# Plots
+spplot(nlMunicipality, 'year', main = 'Year NDVI avg')
+spplot(nlMunicipality, 'January', main = 'January NDVI avg')
+spplot(nlMunicipality, 'August', main = 'August NDVI avg')
 
