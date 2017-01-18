0751c693a4087098241cad5a21092e0564914a60
rodrigoalmeida94 <rodrigo.almeida94@outlook.pt>
2017-01-18 16:51:55 +0100

    Almost done

--- Lesson8/main.R
+++ Lesson8/main.R
@@ -10,19 +10,53 @@ for (name in list.files('data/', full.names = T)){
   load(name)
 }
 
+# Clean up data
+vcfGewata[vcfGewata>100 | vcfGewata<0 ] <- NA
+GewataB1[GewataB1>700 | GewataB1 <200] <- NA
+GewataB2[GewataB2>700 | GewataB2 <200] <- NA
+GewataB3[GewataB3>900 | GewataB3 <200] <- NA
+GewataB4[GewataB4>3200 | GewataB4 <1500] <- NA
+GewataB5[GewataB5>3000 | GewataB5 <900] <- NA
+GewataB7[GewataB7>2000 | GewataB7 <300] <- NA
+
 # Creates a brick
 gewata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
 
 # Gives names to layers in brick
 names(gewata) <- c('band1', 'band2', 'band3', 'band4', 'band5', 'band7', 'VCF')
 
+# Plots relashionships between bands 
+pairs(gewata)
+
 # Extract data to a data frame
 gewataDF <- as.data.frame(getValues(gewata))
 
-# Remove outliers and change scale
-gewataDF[1:6] <- gewataDF[1:6]/10000
-gewataDF$VCF[gewataDF[7] > 100] <- NA
+# Create model
+model <- lm(VCF ~ band1 + band2 + band3 + band4 + band5 + band7, data = gewataDF)
+summary(model)
 
-# Plots relashionships between bands 
-pairs(gewata)
+# Subset predictors
+gewataDF_pred <- gewataDF[1:6]
+
+# Get the predictions
+VCF_pred <- predict(model, gewataDF_pred, na.rm = T)
+
+# Gets the prediction in the brick
+gewata$VCF_predict <- VCF_pred
+gewataDF$VCF_predict <- VCF_pred
+
+# Display 
+par(mfrow = c(3,1))
+plot(gewata$VCF, main = 'VCF')
+plot(gewata$VCF_predict, main = 'Predicted VCF')
+plot(gewata$VCF_predict - gewata$VCF, main = 'VCF difference')
+par(mfrow = c(1,1))
+
+# Calculate RMSE
+RMSE <- sqrt(sum((gewataDF$VCF_predict - gewataDF$VCF)^2,na.rm = T)/nrow(gewataDF))
+print(RMSE)
+
+
+
+
 
