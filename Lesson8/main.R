d167cdb12dbc74a6d5e8a7421fbb89df1d6f12dc
rodrigoalmeida94 <rodrigo.almeida94@outlook.pt>
2017-01-19 09:53:29 +0100

    knitr added

--- Lesson8/main.R
+++ Lesson8/main.R
@@ -2,6 +2,7 @@
 # Team Dragon Masters
 # 18/01/2017
 
+# ---- part1 ----
 # Libraries and sources
 library(raster)
 
@@ -10,18 +11,14 @@ for (name in list.files('data/', full.names = T)){
   load(name)
 }
 
-#REMOVE LATER
-load('data/trainingPoly.rda')
-
-
 # Clean up data
-vcfGewata[vcfGewata>100 | vcfGewata<0 ] <- NA
-GewataB1[GewataB1>700 | GewataB1 <200] <- NA
-GewataB2[GewataB2>700 | GewataB2 <200] <- NA
-GewataB3[GewataB3>900 | GewataB3 <200] <- NA
-GewataB4[GewataB4>3200 | GewataB4 <1500] <- NA
-GewataB5[GewataB5>3000 | GewataB5 <900] <- NA
-GewataB7[GewataB7>2000 | GewataB7 <300] <- NA
+vcfGewata[vcfGewata > 100 | vcfGewata < 0] <- NA
+GewataB1[GewataB1 > 700 | GewataB1 < 200] <- NA
+GewataB2[GewataB2 > 700 | GewataB2 < 200] <- NA
+GewataB3[GewataB3 > 900 | GewataB3 < 200] <- NA
+GewataB4[GewataB4 > 3200 | GewataB4 < 1500] <- NA
+GewataB5[GewataB5 > 3000 | GewataB5 < 900] <- NA
+GewataB7[GewataB7 > 2000 | GewataB7 < 300] <- NA
 
 # Creates a brick
 gewata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
@@ -50,14 +47,12 @@ gewata$VCF_predict <- VCF_pred
 gewataDF$VCF_predict <- VCF_pred
 
 # Display 
-par(mfrow = c(3,1))
 plot(gewata$VCF, main = 'VCF')
 plot(gewata$VCF_predict, main = 'Predicted VCF')
 
 # Calculates and displays the difference
 gewata$VCF_diff <- gewata$VCF_predict - gewata$VCF
 plot(gewata$VCF_diff, main = 'VCF difference')
-par(mfrow = c(1,1))
 
 # Calculate global RMSE
 RMSE <- sqrt(sum((gewataDF$VCF_predict - gewataDF$VCF)^2,na.rm = T)/nrow(gewataDF))
@@ -68,9 +63,16 @@ trainingPoly@data$Code <- as.numeric(trainingPoly@data$Class)
 trainingRaster <- rasterize(trainingPoly, gewata, field = 'Code')
 
 # Create squared difference
-gewata$VCF_diff2 <- (gewata$VCF_predict - gewata$VCF)^2
+gewata$VCF_diff2 <- (gewata$VCF_diff)^2
 
 # Make zonal statistics
 RMSE_classes <- zonal(gewata$VCF_diff2, trainingRaster, fun = mean)
+
+# Calcuates the SQRT
 RMSE_classes[,2] <- sqrt(RMSE_classes[,2])
+
+# Gives proper formatting to matrix
+colnames(RMSE_classes) <- c('class', 'RMSE')
+class_names <- levels(trainingPoly@data$Class)
+RMSE_classes[,1] <- class_names[RMSE_classes[,1]]
 print(RMSE_classes)
