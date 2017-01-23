d1804d1d164e60801ecd1778546d80e21d33367c
rodrigoalmeida94 <rodrigo.almeida94@outlook.pt>
2017-01-23 11:29:22 +0100

    first commit lesson 9

---
+++ Lesson9/answers.md
@@ -0,0 +1,2 @@
+#VectorPython Questions
+##Question 1: On the previous example where WKT is printed out, in which coordinate system is the WKT represented?
---
+++ Lesson9/main.py
@@ -0,0 +1,35 @@
+from osgeo import ogr, osr
+import os
+
+os.chdir('/home/ubuntu/userdata/GeoscriptingGitSARA/Lesson9/')
+
+driverName = "ESRI Shapefile"
+drv = ogr.GetDriverByName( driverName )
+
+a_file = 'data/points.shp'
+layername = 'a_layer'
+
+ds = drv.CreateDataSource(a_file)
+
+spatialReference = osr.SpatialReference()
+spatialReference.ImportFromEPSG(4326)
+
+layer = ds.CreateLayer(layername, spatialReference, ogr.wkbPoint)
+
+kml_file = ogr.Open('data/points.kml')
+
+for lay in kml_file:
+    for feat in lay:
+        geom = feat.GetGeometryRef()
+        if geom != None:
+            point = []
+            for i in range(0, geom.GetPointCount()):
+                coords = geom.GetPoint(i)
+                point = ogr.Geometry(ogr.wkbPoint)
+                point.SetPoint(0,coords[0],coords[1])
+                layerDef = layer.GetLayerDefn()
+                feature = ogr.Feature(layerDef)
+                feature.SetGeometry(point)
+                layer.CreateFeature(feature)
+
+ds.Destroy()
